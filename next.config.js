7ff59d9094a293a7dd9ca6f45b2ee48367bf5abd
methe ezzitouni <methe@walo.app>
2022-08-07 16:33:21 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/portfolio/', 
+  assetPrefix: 'https://methe-1.github.io/portfolio/',
   basePath: '/portfolio',
   images: {
     loader: 'akamai',
