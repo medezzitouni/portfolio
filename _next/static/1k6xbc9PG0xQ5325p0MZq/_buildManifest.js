b1cd5c3d9a098032cdcd410437f61adb247bf6d9
methe ezzitouni <methe@walo.app>
2022-08-07 16:06:50 +0100

    exported next app

--- _next/static/1k6xbc9PG0xQ5325p0MZq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bece3e60a6afad3b.css","static/chunks/pages/index-49d78e3002060892.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
