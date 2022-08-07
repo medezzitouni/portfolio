7198480bbf2140fb84eeab34f5609822fe02325a
methe ezzitouni <methe@walo.app>
2022-08-07 15:51:05 +0100

    moved comingSoon to be a componennt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Layout from '../components/Layout'
-import ComingSoon from './comingSoon'
+import ComingSoon from '../components/comingSoon'
 
 const Home: NextPage = () => {
   return (
