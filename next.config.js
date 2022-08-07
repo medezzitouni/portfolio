398b78f27642d75341083b241a874d06270cd845
methe ezzitouni <methe@walo.app>
2022-08-07 16:11:40 +0100

    added basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/portfolio/',
+  assetPrefix: './',
   basePath: '/portfolio',
   images: {
     loader: 'akamai',
