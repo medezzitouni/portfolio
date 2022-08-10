9f011f0267e259198e6c879e6e8a1703b2bf0205
methe ezzitouni <methe@walo.app>
2022-08-10 19:04:47 +0100

    Deploy gh-pages

---
+++ out/_next/static/chunks/1a48c3c1-1b2623d33445c2ed.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[874],{7516:function(t,n,u){u.d(n,{lU2:function(){return e}});var a=u(8357);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"}}]})(t)}}}]);
