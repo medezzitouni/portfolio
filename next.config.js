89cded4ceaad2c2b643fe499b9e24cf70afd611a
methe ezzitouni <methe@walo.app>
2022-08-10 19:33:14 +0100

    fixed a bug

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const { ENV } = process.env
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: ENV == 'prod' ? '/portfolio/' : '',
-  basePath: ENV == 'prod' ? '/portfolio/' : '',
+  basePath: ENV == 'prod' ? '/portfolio' : '',
   images: {
     loader: 'imgix',
     path: ''
