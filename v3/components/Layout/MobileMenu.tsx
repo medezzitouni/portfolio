3b3bd6814e665d1e87b4514336ce680022d37853
methe ezzitouni <methe@walo.app>
2023-01-15 00:11:32 +0100

    :books: moved the ScrollingSetup to lib

--- v3/components/Layout/MobileMenu.tsx
+++ v3/components/Layout/MobileMenu.tsx
@@ -3,56 +3,9 @@ import { Page } from "@types";
 import Link from "next/link";
 import Category from "../Category";
 import { useCallback } from "react";
-import styles from '~styles/mobile_menu.module.css';
+import { cn, setupScrolling } from '~lib/utils';
 
-const setupScrolling = () => {
-  var keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
-  function preventDefault(e: any) {
-    e.preventDefault();
-  }
-
-function preventDefaultForScrollKeys(e: any) {
-  // @ts-ignore
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
-}
-
-// modern Chrome requires { passive: false } when adding event
-var supportsPassive = false;
-try {
-  // @ts-ignore
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-  }));
-} catch(e) {}
-
-var wheelOpt = supportsPassive ? { passive: false } : false;
-// // @ts-ignore
-
-var wheelEvent = typeof window === "object" && 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-  // call this to Disable
-  function disableScroll() {
-    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-  }
-
-  // call this to Enable
-  function enableScroll() {
-    window.removeEventListener('DOMMouseScroll', preventDefault, false);
-    // @ts-ignore
-    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-    // @ts-ignore
-    window.removeEventListener('touchmove', preventDefault, wheelOpt);
-    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-  }
-  return [disableScroll, enableScroll];
-}
 
 const [disable, enable] = setupScrolling();
 
@@ -134,7 +87,8 @@ export const MobileMenu: FunctionComponent = () => {
                     X
                 </div>
             </div>
-           { menu == 'href' ? <ul className="flex flex-col gap-3 font-bold text-2xl  grow items-center justify-center">
+            {/* menu == 'href'  */}
+            <ul className={cn( menu == 'filter' ? 'hidden' : 'flex' , 'flex-col gap-3 font-bold text-2xl  grow items-center justify-center')}>
                 {pages.map((page, index) => (
                     <li
                         key={index}
@@ -149,12 +103,11 @@ export const MobileMenu: FunctionComponent = () => {
                 </li>
                 ))}
             </ul>
-            :
-            <div className={`flex flex-col gap-3 font-bold text-2xl  grow items-center justify-center`}>
+            <div className={cn( menu == 'href' ? 'hidden' : 'flex' ,`flex-col gap-3 font-bold text-2xl  grow items-center justify-center`)}>
                 <Category clear={() => { setTimeout(() => handleMenu(''), 300)}} 
                 bg_active='bg-white text-black' bg_inactive='bg-primary bg-opacity-60 text-white' />
                 {/* <CommingSoon pathname={false} /> */}
-            </div>}
+            </div>
         </div>
       </div>
     </nav>
--- v3/lib/utils.ts
+++ v3/lib/utils.ts
@@ -5,3 +5,54 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+
+export const setupScrolling = () => {
+  var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+  function preventDefault(e: any) {
+    e.preventDefault();
+  }
+
+function preventDefaultForScrollKeys(e: any) {
+  // @ts-ignore
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  // @ts-ignore
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+// // @ts-ignore
+
+var wheelEvent = typeof window === "object" && 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+  // call this to Disable
+  function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+  }
+
+  // call this to Enable
+  function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    // @ts-ignore
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    // @ts-ignore
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+  }
+  return [disableScroll, enableScroll];
+}
+
