a45bb749d7389cd3b131789e47788e80d3846b90
methe ezzitouni <methe@walo.app>
2023-01-22 01:03:56 +0100

    :sparkle: import fonts depending on the env

--- v3/pages/_app.tsx
+++ v3/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '~styles/globals.css'
 import type { AppProps } from 'next/app'
 import { wrapper } from '~store'
+
+if(process.env.BACKEND_URL == '/portfolio/')
+  import('~styles/portfolioFonts.css');
+else
+  import('~styles/fonts.css');
+  
+import '~styles/globals.css'
+
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
