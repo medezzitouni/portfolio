8b9e6cf3f7a9836e3d4b55c64030f966e3b93353
methe ezzitouni <methe@walo.app>
2023-02-11 23:52:53 +0100

    Deploy v3-deploy

--- v3/lib/constants.ts
+++ v3/lib/constants.ts
@@ -1,6 +1,6 @@
 
 export const IG_API_URL = process.env.NEXT_PUBLIC_IG_BASE_URL;
-export const IG_TOKEN = process.env.NEXT_PUBLIC_IG_TOKEN || '';
+export const IG_TOKEN = process.env.IG_TOKEN || '';
 export const BASE_URL = process.env.NEXT_PUBLIC_APP_BASE_URL || '';
 export const DEBUG = process.env.NEXT_PUBLIC_DEBUG == 'true' ? true : false;
 export const VERCEL_FUNCTIONS_BASE_URL = process.env.NEXT_PUBLIC_VERCEL_FUNCTIONS_BASE_URL;
